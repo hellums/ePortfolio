94d679a438a725eed341fdc00cccc0dcf14d516b
hellums <83464025+hellums@users.noreply.github.com>
2022-06-07 16:47:14 -0400

    Moved code behind to class file for Projects

---
+++ Pages/Projects.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+
+namespace ePortfolio.Pages
+{
+    public partial class Projects
+    {
+        private Project[]? projects;
+        protected override async Task OnInitializedAsync()
+        {
+            projects = await Http.GetFromJsonAsync<Project[]>("data/projects.json");
+        }
+
+        public class Project
+        {
+            public string Customer { get; set; }
+            public string System { get; set; }
+            public string Language { get; set; }
+
+            public string GetCustomer()
+            { return (Customer); }
+
+            public string GetSystem()
+            { return (System); }
+
+            public string GetLanguage()
+            { return (Language); }
+        }
+    }
+}
